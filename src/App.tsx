242dd7e339f3112e5792d269977f26774b6a698c
darwin <darwin.apolinario@serino.com>
2021-08-23 22:11:13 +0800

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  /*  const weqw = "qweqwe"; */
+  /* const weqw = "qweqwe"; */
   return (
     <div className="App">
       <header className="App-header">
